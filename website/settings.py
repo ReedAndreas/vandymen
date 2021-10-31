2489987f2a72ded43542ee82d8d41fbb7c6f4ed2
Reed Andreas <79937667+ReedAndreas@users.noreply.github.com>
2021-10-31 11:57:49 -0500

    Updates

--- website/settings.py
+++ website/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['eb-django-app-dev.elasticbeanstalk.com']
 
 ROOT_URLCONF = 'website.urls'
 
