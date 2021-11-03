c166e7f99fc2fe55502f98377d491fcdd9568b20
Reed Andreas <79937667+ReedAndreas@users.noreply.github.com>
2021-11-02 20:00:20 -0500

    Update Mock Draft

--- statsApp/df_manipulation.py
+++ statsApp/df_manipulation.py
@@ -12,19 +12,19 @@ def get_df(players):
 def get_erg(players, teams):
     df = get_df(players)
 
+    df = df[df['ab'] > 5]
+
     team_data = []
     for t in teams:
         num_players = 0
-        vals = {'aH': 0, 'aHR' : 0, 'aW' : 0, 'aR' : 0}
+        vals = {'aH': 0, 'aHR' : 0, 'aR' : 0}
         for index, row in df[df['team_id'] == t.pk].iterrows():
             vals['aH'] += row['h']
             vals['aHR'] += row['hr']
-            vals['aW'] += row['bb']
             vals['aR'] += row['rbi']
             num_players += 1
         vals['aH'] /= num_players
         vals['aHR'] /= num_players
-        vals['aW'] /= num_players
         vals['aR'] /= num_players
         team_data.append(vals)
 
@@ -37,13 +37,13 @@ def get_erg(players, teams):
     lr = LinearRegression()
     lr.fit(X, y)
 
-    simple_players = df[['name','ab','ba', 'hr', 'bb']]
+    simple_players = df[['name','ab','ba', 'hr']]
     simple_players['hr'] = simple_players.apply(lambda x : x['hr']/x['ab'] if x['ab'] > 0 else 0, axis = 1)
-    simple_players['bb'] = simple_players.apply(lambda x : x['bb']/x['ab'] if x['ab'] > 0 else 0, axis = 1)
 
     simple_players = simple_players.drop(['name', 'ab'], axis = 1)
     df = df.reset_index().drop('index', axis = 1)
-    simple_players.columns = ['aH', 'aHR', 'aW']
+    simple_players.columns = ['aH', 'aHR']
     predictions = lr.predict(simple_players)
     df['E_Runs_Generated'] = pd.DataFrame(predictions)
+    df['E_Runs_Generated'] = df.apply(lambda x : x['E_Runs_Generated'] * np.sqrt(x['rbi']), axis = 1)
     return df.sort_values('E_Runs_Generated', ascending = False)
